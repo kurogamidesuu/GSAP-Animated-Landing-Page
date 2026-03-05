e35479c06902daab6ea5bc0f19d0e379c22ea60f
kurogamidesuu <hempushpchauhan@gmail.com>
2026-03-05 18:22:19 +0530

    implement the first part of the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <div className="text-3xl text-indigo-300">Hello, GSAP!</div>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black" />
+    </main>
   )
 }
 
